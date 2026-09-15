0f7c69bbd16fe4e323ac186eea0da85292e3e19a
Yerrox <pawelopon2@gmail.com>
2026-09-15 11:27:02 +0200

    eggggg7

--- skrypt.js
+++ skrypt.js
@@ -1,36 +1,35 @@
-const tab = [];
+const rejestrAmplitud = new Array();
 
-const btn = document.getElementById('btn-dodaj');
-
+const btn = document.getElementById('btn-zapisz');
 btn.addEventListener('click', () => {
 
-    const nazProdukt = document.getElementById('nazprodukt').value.trim();
-    const sztuki = parseInt(document.getElementById('sztuk').value, 10);
-    const cenaJed = parseFloat(document.getElementById('cena').value);
-    
-    if (isNaN(sztuki) || isNaN(cenaJed) ) {
-        alert("BŁĄD! Podane dane są nieprawidłowe.");
-        return;     
+    const rejonWys = document.getElementById('rejon').value;
+    const amplituda = parseFloat(document.getElementById('amplituda').value);
+    const czas = parseInt(document.getElementById('czas').value, 10);
+
+    if (rejonWys === '' || isNaN(amplituda) || isNaN(czas)) {
+        alert('Błąd wprowadzania: Uzupełnij poprawnie wszystkie parametry.');
+        return;
     }
 
-    let wartoscCalkowita = sztuki * cenaJed;
+    rejestrAmplitud.push(amplituda);
 
-    tab.push(wartoscCalkowita);
+    const energiaSkumulowana = amplituda * czas;
 
-    const cialoTabeli = document.getElementsByTagName('tbody')[0];
-    
-    const noweWiersze = `<tr><td>${nazProdukt}</td><td>${sztuki}</td><td>${wartoscCalkowita}</td></tr>`;
+    const cialoTabeli = document.querySelector('table tbody');
+    const nowyWiersz = `<tr><td>${rejonWys}</td><td>${amplituda}</td><td>${energiaSkumulowana}</td></tr>`;
 
-    cialoTabeli.innerHTML += noweWiersze;
+    cialoTabeli.innerHTML += nowyWiersz;
 
-    let akumulator = 0;
-    for (let i = 0; i < tab.length; i++) {
-        const elementy = tab[i];
-        akumulator += elementy;
+    let wskaznikZdarzen = 0;
+    for (let i = 0; i < rejestrAmplitud.length; i++) {
+        const elementy = rejestrAmplitud[i];
+        
+        if (elementy >= 5) {
+            wskaznikZdarzen++;
+        }
     }
 
-    const suma = document.getElementById('suma-wyjscie');
-    if (suma) {
-        suma.textContent = akumulator;
-    }
+    const p = document.getElementById('statystyki');
+    p.innerText = `Zajerestrowano łącznie ${rejestrAmplitud.length} wstrząsów. Zdarzenia krytyczne ${wskaznikZdarzen}`;
 });
