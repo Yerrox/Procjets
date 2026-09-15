ad18fecb6183ed27a6d941fe35110b742e3067d7
Yerrox <pawelopon2@gmail.com>
2026-09-15 14:32:59 +0200

    dds3

--- skrypt.js
+++ skrypt.js
@@ -16,7 +16,7 @@ btn.addEventListener('click', () => {
 
     const energiaSkumulowana = amplituda * czas;
 
-    const cialoTabeli = document.querySelector('tbody');
+    const cialoTabeli = document.querySelector('table tbody');
     const nowyWiersz = `<tr><td>${rejonWys}</td><td>${amplituda}</td><td>${energiaSkumulowana}</td></tr>`;
 
     cialoTabeli.innerHTML += nowyWiersz;
