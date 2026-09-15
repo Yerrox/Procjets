2052c2917360cb807526de43e745e90bed613517
Yerrox <pawelopon2@gmail.com>
2026-09-15 10:05:01 +0200

    ext6

--- skrypt.js
+++ skrypt.js
@@ -8,4 +8,24 @@ btn.addEventListener('click', () => {
     const sztuki = parseInt(10, document.getElementById('sztuk').value);
     const cenaJed = parseFloat(document.getElementById('cena').value);
     
+    if (isNaN(sztuki) || isNaN(cenaJed) ) {
+        alert("BŁĄD! Podane dane są nieprawidłowe.");
+        return;     
+    }
+
+    let wartoscCalkowita = sztuki * cenaJed;
+
+    tab.push(wartoscCalkowita);
+
+    const cialoTabeli = document.getElementsByTagName('tbody');
+    
+    const noweWiersze = `<tr><td>${nazProdukt}</td></tr><tr><td>${sztuki}</td></tr><tr><td>${wartoscCalkowita}</td></tr>`;
+
+    cialoTabeli.innerHTML += noweWiersze;
+
+    let akumulator = 0;
+    for (let i = 0; i < tab.length; i++) {
+        const elementy = tab[i];
+        akumulator += elementy;
+    }
 });
