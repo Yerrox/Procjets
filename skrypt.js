e8830dd44a893aeedff1677ec42b1f765376cdd8
Yerrox <pawelopon2@gmail.com>
2026-09-15 10:18:49 +0200

    ex7

--- skrypt.js
+++ skrypt.js
@@ -4,8 +4,8 @@ const btn = document.getElementById('btn-dodaj');
 
 btn.addEventListener('click', () => {
 
-    const nazProdukt = document.getElementById('nazprodukt').value.trim;
-    const sztuki = parseInt(10, document.getElementById('sztuk').value);
+    const nazProdukt = document.getElementById('nazprodukt').value.trim();
+    const sztuki = parseInt(document.getElementById('sztuk').value, 10);
     const cenaJed = parseFloat(document.getElementById('cena').value);
     
     if (isNaN(sztuki) || isNaN(cenaJed) ) {
@@ -17,7 +17,7 @@ btn.addEventListener('click', () => {
 
     tab.push(wartoscCalkowita);
 
-    const cialoTabeli = document.getElementsByTagName('tbody');
+    const cialoTabeli = document.getElementsByTagName('tbody')[0];
     
     const noweWiersze = `<tr><td>${nazProdukt}</td></tr><tr><td>${sztuki}</td></tr><tr><td>${wartoscCalkowita}</td></tr>`;
 
@@ -27,5 +27,10 @@ btn.addEventListener('click', () => {
     for (let i = 0; i < tab.length; i++) {
         const elementy = tab[i];
         akumulator += elementy;
+    }
+
+    const suma = document.getElementById('suma-wyjscie');
+    if (suma) {
+        suma.textContent = akumulator;
     }
 });
