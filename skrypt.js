b09a732b80811edc8ee9c7455a3699e7ebb3286d
Yerrox <pawelopon2@gmail.com>
2026-09-15 09:31:59 +0200

    ext5

---
+++ skrypt.js
@@ -0,0 +1,11 @@
+const tab = [];
+
+const btn = document.getElementById('btn-dodaj');
+
+btn.addEventListener('click', () => {
+
+    const nazProdukt = document.getElementById('nazprodukt').value.trim;
+    const sztuki = parseInt(10, document.getElementById('sztuk').value);
+    const cenaJed = parseFloat(document.getElementById('cena').value);
+    
+});
