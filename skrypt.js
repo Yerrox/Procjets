9275a9bc251fc2897d3466d79582023784f8f5f9
Yerrox <pawelopon2@gmail.com>
2026-09-16 11:24:54 +0200

    exty88

---
+++ skrypt.js
@@ -0,0 +1,33 @@
+const rejestrZuzycia = new Array();
+
+const b = document.getElementById('btn');
+b.addEventListener('click', () =>{
+    
+    const f = document.forms['form'];
+    const nazwaSprzetu = f['sprzet'].value.trim();
+    const poborMocy = parseFloat(f['pobor'].value);
+    const czas = parseFloat(f['czas'].value);
+
+    if (isNaN(poborMocy) || isNaN(czas)) {
+        alert('BŁĄD! Niepoprawne dane.');
+        return;
+    }
+
+    const zuzycieEnergii = (poborMocy * czas) / 1000;
+
+    rejestrZuzycia.push(zuzycieEnergii);
+
+    const cialoTabeli = document.querySelector('table tbody');
+    const nowyWiersz = `<tr><td> ${nazwaSprzetu} </td><td> ${zuzycieEnergii} </td><td> ${zuzycieEnergii > 5 ? 'WYSOKIE' : 'W NORMIE'} </td></tr>`;
+
+    cialoTabeli.innerHTML =+ nowyWiersz;
+
+    let akumulator = 0;
+    for (let i = 0; i < rejestrZuzycia.length; i++) {
+        const element = rejestrZuzycia[i];
+        akumulator += element;
+    }
+
+    const p = document.getElementById('wynik');
+    p.textContent = `Zarejestrowano ${element} urządzeń. Całkowite zużycie: ${akumulator} kWh.`;
+});
