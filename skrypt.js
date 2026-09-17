1bf5c3b2b5c9abbf6527ff1e059f8c0ab9deae5d
Yerrox <pawelopon2@gmail.com>
2026-09-17 09:19:29 +0200

    extt66

--- skrypt.js
+++ skrypt.js
@@ -1,9 +1,10 @@
-const b = document.querySelector('.panel-wejscia button');
-b.addEventListener('click', () =>{
+const b = document.querySelector('.btn-dekoduj');
+
+b.addEventListener('click', () => {
     const ciagZnakow = document.getElementById('sygnal').value.trim();
 
     if (ciagZnakow.length < 15) {
-        alert('Błąd: Sygnału jest zbyt krótki!');
+        alert('Błąd: Sygnał jest zbyt krótki!');
         return;
     }
 
@@ -14,27 +15,23 @@ b.addEventListener('click', () =>{
     const filtracjaPole = document.getElementById('out-podmiana');
     const drugieSlowoPole = document.getElementById('out-slowo');
 
-    const zmiana1 = ciagZnakow.toUpperCase();
-    wielkieLiteryPole.innerText = zmiana1;
+    const wielkiSygnal = ciagZnakow.toUpperCase();
+    wielkieLiteryPole.innerText = wielkiSygnal;
 
-    const znak5 = ciagZnakow.charAt(4);
-    const znakAscii = ciagZnakow.charCodeAt(4);
+    const znak5 = wielkiSygnal.charAt(4);
+    const znakAscii = wielkiSygnal.charCodeAt(4);
     piatyZnakPole.textContent = `Znak: ${znak5}, ASCII: ${znakAscii}`;
 
-    const fraza = ciagZnakow.toUpperCase().indexOf('INF');
+    const fraza = wielkiSygnal.indexOf('INF');
     pozycjaInfPole.innerText = fraza;
 
-    const wycinekZdania = ciagZnakow.substring(4, 11);
+    const wycinekZdania = wielkiSygnal.substring(4, 11);
     wycinekPole.textContent = wycinekZdania;
 
-    const podmianka = ciagZnakow.replace(/0/g, '0');
-
+    const podmianka = wielkiSygnal.replace(/0/g, 'O');
     filtracjaPole.innerText = podmianka;
 
-    const separacja = ciagZnakow.split(" ");
-
-    const drugieSlowo = separacja[1];
+    const separacja = wielkiSygnal.split(" ");
+    const drugieSlowo = separacja[1] ? separacja[1] : "";
     drugieSlowoPole.innerText = drugieSlowo;
-
-
 });
