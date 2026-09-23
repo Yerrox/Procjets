361cc1c6855e724f947f15cb02e85ae53a6e7fc1
Yerrox <pawelopon2@gmail.com>
2026-09-23 11:55:58 +0200

    exef6

--- skrypt.js
+++ skrypt.js
@@ -7,7 +7,9 @@ btn.addEventListener('click', () => {
     const poleStatus = document.getElementById('wynik-status');
     const poleIdentyfikator = document.getElementById('identyfikator');
     const poleKwota = document.getElementById('kwota');
-    const poleCenzura = document.getElementById('kwota')
+    const poleCenzura = document.getElementById('kwota');
+
+    const polePanelWynikow = document.getElementById('panel-wynikow');
 
     if (daneKlientaW.length === 0 || isNaN(wartoscZamowienie) || wartoscZamowienie <= 0) {
         alert('BŁĄD: Wprowadź prawidłowe dane zamówienia.');
@@ -33,6 +35,21 @@ btn.addEventListener('click', () => {
     }
 
     if (kodPromocyjnyW.length === 5 && akumulator === 1) {
-        
-    }else if (kodPromocyjnyW.length !== 5 && akumulator !== 1)
+        polePanelWynikow.style.backgroundColor = "#12331f";
+        polePanelWynikow.style.border = "1px solid #2ecc71";
+        const suma = wartoscZamowienie * 0.8;
+        poleKwota.textContent = suma;
+        poleStatus.textContent = `ZNIŻKA AKTYWNA`;
+        poleStatus.style.color = "#2ecc71";
+    }else if (kodPromocyjnyW.length !== 5 && akumulator !== 1){
+        polePanelWynikow.style.backgroundColor = "#331212";
+        polePanelWynikow.style.border = "1px solid #e74c3c";
+        const suma = wartoscZamowienie * 0.8;
+        poleKwota.textContent = suma;
+        poleStatus.textContent = `KOD NIEPRAWIDŁOWY`;
+        poleStatus.style.color = "#e74c3c";
+    }
+
+    const zamiana = kodPromocyjnyW.replace(/X/i);
+    poleCenzura.textContent = zamiana;
 });
